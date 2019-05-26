f45a92c8e4c592038f2eb3055a1de4defee096bb
sharonmaswai <chepmaswai@gmail.com>
2019-05-26 23:11:34 +0300

    create an about.html file, add content

--- app/main/views.py
+++ app/main/views.py
@@ -8,3 +8,8 @@ def index():
     name="!Pitch"
     title="Welcome| Pitch your idea in one minutes!"
     return render_template('index.html', name=name)
+@main.route('/about')
+def about_app():
+    message="This is an app where you get to pitch your idea for one minute then other users can rate yout pitch. You also get to rate other people's idea. How cool is that!!!"
+
+    return render_template('about.html',messgae=message)
