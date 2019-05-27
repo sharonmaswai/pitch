71d1d1ba29d7abc5f4ac1b152ba3f5d8d32d001f
sharonmaswai <chepmaswai@gmail.com>
2019-05-27 12:07:47 +0300

    create user tests

--- app/models.py
+++ app/models.py
@@ -1,4 +1,6 @@
 from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
+
 
 
 class User(db.Model):
@@ -8,7 +10,18 @@ class User(db.Model):
     username = db.Column(db.String(255))
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
     pass_secure = db.Column(db.String(255))
-    
+    @property
+        def password(self):
+            raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+        def password(self, password):
+            self.pass_secure = generate_password_hash(password)
+
+
+        def verify_password(self,password):
+            return check_password_hash(self.pass_secure,password)
+
 
     def __repr__(self):
         return f'User {self.username}'
