02f5681bf142305b1da384e8e0ca20c0f5d70909
sharonmaswai <chepmaswai@gmail.com>
2019-05-27 16:58:40 +0300

    add scret key to start file

--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, abort, request,redirect,url_for
 from . import main
 from flask_login import login_required
 # Views
@@ -13,3 +13,27 @@ def about_app():
     message="This is an app where you get to pitch your idea for one minute then other users can rate yout pitch. You also get to rate other people's idea. How cool is that!!!"
 
     return render_template('about.html',message=message)
+
+@main.route('/piches')
+def see_pitches():
+
+    name="!Pitch"
+    title="Welcome| Pitch your idea in one minutes!"
+   
+
+    return render_template('pitches.html',title=title, name=name)
+
+@main.route('/pitches/comments/')
+@login_required
+def post_comment():
+
+    return render_template ('comment.html', title=title, name=name)
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
--- config.py
+++ config.py
@@ -2,6 +2,7 @@ import os
 
 class Config:
 
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://sharon-maswai:qwerty@localhost/pitch'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
