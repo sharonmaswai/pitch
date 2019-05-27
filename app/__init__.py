deb209c49927cfb7dc53a5e5ff45a5c3633f7188
sharonmaswai <chepmaswai@gmail.com>
2019-05-27 12:29:54 +0300

    create pitch route in app/views

--- app/__init__.py
+++ app/__init__.py
@@ -24,6 +24,7 @@ def create_app(config_name):
     # Initializing flask extensions
     bootstrap.init_app(app)
     db.init_app(app)
+    login_manager.init_app(app)
 
     # Registering the blueprint
     from .main import main as main_blueprint
