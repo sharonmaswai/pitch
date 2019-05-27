c578fcedb644e2be69e3e09efdc87d5ea0a93d5b
sharonmaswai <chepmaswai@gmail.com>
2019-05-27 12:15:11 +0300

    install flask login extension and initialise it

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,12 @@ from flask import Flask
 from config import config_options,Config
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
