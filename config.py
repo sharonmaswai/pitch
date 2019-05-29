0b62c7dd2645db599f16f8d9148d7ad58f9b221c
sharonmaswai <chepmaswai@gmail.com>
2019-05-29 18:20:47 +0300

    heroku commits

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 class Config:
 
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://sharon-maswai:qwerty@localhost/pitch'
+    SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://sharon-maswai:qwerty@localhost/pitches'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOADED_PHOTOS_DEST ='app/static/photos'
     # email configurations
